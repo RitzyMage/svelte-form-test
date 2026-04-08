c4215a6f2661bcbb2fe4913bf40e71da43201b0d
Avery Green <averyg@utahcounty.gov>
2026-04-08 07:53:23 -0600

    db schema for test form

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,17 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const task = sqliteTable('task', {
+const GAMES = ['Dominion', 'Magic: The Gathering', 'Slay The Spire', 'Astrea: Six-Sided Oracles', 'Balatro', 'Cobalt Core'] as const;
+type GAME = typeof GAMES[number];
+
+const GUILDS = ['Azorius', 'Selesnya', 'Boros', 'Orzhov', 'Simic', 'Gruul', 'Golgari', 'Dimir', 'Izzet', 'Rakdos'] as const;
+type GUILD = typeof GUILDS[number];
+
+export const userData = sqliteTable('users', {
 	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+	username: text().notNull(),
+	title: text({enum: ["Lord", "Lady"],}),
+	games: text({mode: 'json'}).$type<GAME[]>(),
+	commanders: text({mode: 'json'}).$type<string[]>(),
+	favoriteGuild: text({enum: GUILDS }),
+	socialSecurity: text(),
 });
